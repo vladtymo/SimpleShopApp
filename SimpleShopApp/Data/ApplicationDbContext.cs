a9e2593307692829cca72adc8a14171b50b46df1
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-10 19:34:33 +0300

    Add project files.

---
+++ SimpleShopApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleShopApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
