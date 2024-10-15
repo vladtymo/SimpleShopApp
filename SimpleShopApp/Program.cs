269bd20cb6d897cee6dad0fb36a5d6933cf4db5f
boxmykola <boxykola@gmail.com>
2024-10-15 19:46:54 +0300

    Added Google Authentification

--- SimpleShopApp/Program.cs
+++ SimpleShopApp/Program.cs
@@ -3,7 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using SimpleShopApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
 
+services.AddAuthentication().AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+        googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+    });
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
